e910bbcf5c2b94e1bdf9cac9f6a33ceacd792203
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2018-03-26 11:55:42 +0700

    remove logger

--- rasp_bot.py
+++ rasp_bot.py
@@ -19,10 +19,6 @@ class WebhookServer(object):
 def show_today(message):
     group = message.text.upper()
     print(group)
-    f = open('logs/log.txt', 'a')
-    info = {'id': message.chat.id, 'group': group}
-    f.write(str(info) + '\n')
-    f.close()
     if re.search(r'[0-9][А-Яа-я][0-9]([0-9]|[А-Яа-я])', group):
         weeks = scrapper.scrape_rasp(group=group)
         current_week = scrapper.get_current_week(both_weeks=weeks)
@@ -31,7 +27,6 @@ def show_today(message):
         weeks = scrapper.scrape_rasp(group=group)
         current_week = scrapper.get_current_week(both_weeks=weeks)
         text = scrapper.get_current_day(week=current_week)
-    #elif re.search(r'[А-Яа-я]')
     else:
         text = 'Введите номер своей группы!'
     bot.send_message(message.chat.id, text)
